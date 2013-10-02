95d762a8eaed1186545d979c239b8edcc967139f
Azamat Tokhtaev <krik123@gmail.com>
2013-10-02 14:33:05 +0600

    updated version

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from distutils.core import setup
 from setuptools import find_packages
 setup(
     name='django-tesla-client',
-    version='0.1',
+    version='0.1.1',
     author=u'Azamat Tokhtaev',
     author_email='krik123@gmail.com',
     url='https://github.com/ITAttractor/Tesla-Client',
